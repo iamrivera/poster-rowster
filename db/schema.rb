690cc29e4a88d70e7788375b4ad44ac525ef0907
iamrivera <a3m3rivera@gmail.com>
2021-08-03 09:10:24 -0700

    Migrated resource files

---
+++ db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_08_03_160749) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "rating"
+    t.string "language"
+    t.string "director"
+    t.string "producer"
+    t.string "writer"
+    t.string "release"
+    t.string "sales"
+    t.string "runtime"
+    t.integer "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_movies_on_genre_id"
+  end
+
+  create_table "posters", force: :cascade do |t|
+    t.string "lynk"
+    t.string "votes"
+    t.integer "movie_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_posters_on_movie_id"
+  end
+
+  add_foreign_key "movies", "genres"
+  add_foreign_key "posters", "movies"
+end
