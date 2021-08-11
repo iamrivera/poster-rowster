e3c3b9fec4a889fe239dd8aadd59569cab1fd0a2
iamrivera <a3m3rivera@gmail.com>
2021-08-10 20:30:15 -0700

    Creating Genre Cards and Button Functionality

--- index.js
+++ index.js
@@ -1,54 +1,42 @@
-function fetchGenres() {
-    return fetch('http://127.0.0.1:3000/genres') .then(function(response) {
-            return response.json();
-            }).then(function(json) {
-            renderGenres(json);
-            }); }
+//FETCH GENRES & RENDER GENRES
 
-function renderGenres(genres) {
-    const body = document.querySelector('body');
-    // Next steps - Create a div for row to append to append class=col then append div row to body
-    genres.forEach(genres => {
-        const card = document.createElement('div');
-        card.setAttribute("class","card")
-        card.setAttribute("id",`card-${genres.title}`)
-        card.setAttribute("style","width: 18rem;")
-        card.innerHTML = genres.title;
-        body.appendChild(card);
-    });
+function fetchGenres() {
+  fetch("http://127.0.0.1:3000/genres").then(function (
+    response
+  ) {
+    return response.json();
+  }).then(renderGenres);
 }
 
-<div class="row">
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-</div>
+function renderGenres(genres) {
+  const body = document.querySelector("body");
+  // Next steps - Create a div for row to append to append class=col then append div row to body
+  genres.forEach((genres) => {
+    const card = new Card(genres.title);
+    body.appendChild(card.render());
+  });
+}
 
+//CREATING THE CARD CLASS 
+class Card { 
+  constructor(title){
+    this.title = title; 
+  }
 
+  render (){
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.setAttribute("id", `card-${this.title}`);
+    card.textContent = this.title;
+    let btn = document.createElement("button"); 
+    btn.innerHTML = "See Movies";
+    card.append(btn);
+    return card;
+  }
+}
 
-
-
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("The DOM has loaded");
-    fetchGenres();
+//CALLING THE FUNCTIONS
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+  fetchGenres();
 });
