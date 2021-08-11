2984d928053c2fb6abc3eb37eea7c3cc372eff1e
iamrivera <a3m3rivera@gmail.com>
2021-08-11 00:51:48 -0700

    Created movie cards

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const baseURL = "http://127.0.0.1:3000/"
- 
-
-
 //FETCH GENRES & RENDER GENRES
 function fetchGenres() {
   fetch("http://127.0.0.1:3000/genres")
@@ -15,36 +11,88 @@ function renderGenres(genres) {
   const body = document.querySelector("body");
   // Next steps - Create a div for row to append to append class=col then append div row to body
   genres.forEach((genres) => {
-    const card = new Card(genres.title);
+    let card = new GenreCard(genres.title, genres.id);
+    body.appendChild(card.render());
+  });
+}
+
+// //FETCH MOVIES & RENDER MOVIES
+// function fetchMovies() {
+//   fetch("http://127.0.0.1:3000/movies")
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(renderMovies);
+// }
+
+function renderMovies(movies) {
+  const body = document.querySelector("body");
+  // Next steps - Create a div for row to append to append class=col then append div row to body
+  movies.forEach((movies) => {
+    let card = new MovieCard(movies.title);
     body.appendChild(card.render());
   });
 }
 
 
-//CREATING THE CLASSES
 
-class Card {
-  constructor(title) {
+//CREATING THE CLASSES
+// Next step --> Create a Genre Card + Movie Card classes with unique divs so you can easily hide and unhide 
+class GenreCard {
+  constructor(title, id) {
     this.title = title;
+    this.id = id
   }
 
   render() {
-    const card = document.createElement("div");
-    card.setAttribute("class", "card");
-    card.setAttribute("id", `card-${this.title}`);
-    card.textContent = this.title;
-    debugger
+    const gcard = document.createElement("div");
+    gcard.setAttribute("class", "card");
+    gcard.setAttribute("id", `gcard-${this.title}`);
+    gcard.textContent = this.title;
     let btn = document.createElement("button");
     btn.innerHTML = `See ${this.title} Movies`;
-    // btn.addEventListener("click", )
-    card.append(btn);
-    return card;
-  }
-
-  handleGenreClick() {
-    return ``
+    btn.addEventListener("click", function(){
+      fetch(`http://127.0.0.1:3000/${this.id}movies`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(renderMovies);
+    })
+    gcard.append(btn);
+    return gcard;
   }
 }
+
+
+class MovieCard {
+  constructor(title, id) {
+    this.title = title;
+    this.id = id
+  }
+
+  render() {
+    const mcard = document.createElement("div");
+    mcard.setAttribute("class", "card");
+    mcard.setAttribute("id", `mcard-${this.title}`);
+    mcard.textContent = this.title;
+    let btn = document.createElement("button");
+    btn.innerHTML = `See ${this.title} Posters`;
+    btn.addEventListener("click", function(){
+      fetch(`http://127.0.0.1:3000/${this.id}movies`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(renderMovies);
+    })
+    mcard.append(btn);
+    return mcard;
+  }
+}
+
+
+
+
+
 
 //CALLING THE FUNCTIONS
 document.addEventListener("DOMContentLoaded", function () {
