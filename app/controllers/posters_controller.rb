286a15e485f15f52460cb539cf6ee07ca87ec292
iamrivera <a3m3rivera@gmail.com>
2021-08-13 08:33:58 -0700

    Resolved backend rails Poster persist/find_by issue

--- app/controllers/posters_controller.rb
+++ app/controllers/posters_controller.rb
@@ -14,8 +14,9 @@ class PostersController < ApplicationController
     end
 
     def add_vote
-        # byebug Id is not aligned find by 10 give post id of 1 
-        poster = Poster.find_by(params[:id]) 
+        # byebug 
+        #Id is not aligned find by 10 give post id of 1 
+        poster = Poster.find_by(id: params[:id]) 
         if poster
             poster.votes += 1
             poster.save
